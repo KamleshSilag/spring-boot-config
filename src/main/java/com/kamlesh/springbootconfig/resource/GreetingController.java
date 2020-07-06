47f4ebe44ece93fea6052c982dbb29caf81cc55c
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-06 14:49:56 +0530

    initial commit

---
+++ src/main/java/com/kamlesh/springbootconfig/resource/GreetingController.java
@@ -0,0 +1,29 @@
+package com.kamlesh.springbootconfig.resource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GreetingController {
+
+    @Value("${my.greeting : default value}")
+    private String greetingMessage;
+
+    @Value("some random message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Value("#{${dbValues}}")
+    private Map<String ,String > dbValues;
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return greetingMessage + " " + staticMessage + " " + listValues + " " + dbValues;
+    }
+}
