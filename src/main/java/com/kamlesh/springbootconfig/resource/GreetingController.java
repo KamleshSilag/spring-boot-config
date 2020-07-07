e14aee5a7c96a622ba95d4901827c41c5bbcb581
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-07 19:41:19 +0530

    ConfigurationProperties and yaml addition

--- src/main/java/com/kamlesh/springbootconfig/resource/GreetingController.java
+++ src/main/java/com/kamlesh/springbootconfig/resource/GreetingController.java
@@ -1,5 +1,7 @@
 package com.kamlesh.springbootconfig.resource;
 
+import com.kamlesh.springbootconfig.DbSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +21,11 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> listValues;
 
-    @Value("#{${dbValues}}")
-    private Map<String ,String > dbValues;
+    @Autowired
+    private DbSettings dbSettings;
 
     @GetMapping("/greeting")
     public String greeting(){
-        return greetingMessage + " " + staticMessage + " " + listValues + " " + dbValues;
+        return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
     }
 }
