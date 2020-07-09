e2239156337fed4b767d2d8efa618a3c415be750
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-09 15:21:09 +0530

    Adding Spring boot config server

--- src/main/java/com/kamlesh/springbootconfig/resource/GreetingController.java
+++ src/main/java/com/kamlesh/springbootconfig/resource/GreetingController.java
@@ -3,6 +3,7 @@ package com.kamlesh.springbootconfig.resource;
 import com.kamlesh.springbootconfig.DbSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import java.util.Map;
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting : default value}")
+    @Value("${my.greeting}")
     private String greetingMessage;
 
     @Value("some random message")
@@ -26,6 +27,7 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(){
-        return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+        return "greeting : "+greetingMessage+ " db connection:" + dbSettings.getConnection();
     }
+
 }
